4c61b465ff0827d83d58d15c376313e6f52adc70
mileshendricks <nextbit.cs@gmail.com>
2024-12-05 10:53:19 +0600

    Design:- Static design home page.

--- footer.php
+++ footer.php
@@ -10,23 +10,97 @@
  */
 
 ?>
-
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'storebase' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'storebase' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'storebase' ), 'storebase', '<a href="https://wcstudio.com">WC Studio</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+<!-- footer start -->
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="full">
+                    <div class="logo_footer">
+                        <a href="#" style="color: black; font-weight: bold; font-size: 30px;">
+                            StoreBase
+                            <!--                                <img width="210" src="images/logo.png" alt="#" />-->
+                        </a>
+                    </div>
+                    <div class="information_f">
+                        <p><strong>ADDRESS:</strong> 28 White tower, Street Name New York City, USA</p>
+                        <p><strong>TELEPHONE:</strong> +91 987 654 3210</p>
+                        <p><strong>EMAIL:</strong> yourmain@gmail.com</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-8">
+                <div class="row">
+                    <div class="col-md-7">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="widget_menu">
+                                    <h3>Menu</h3>
+                                    <ul>
+                                        <li><a href="#">Home</a></li>
+                                        <li><a href="#">About</a></li>
+                                        <li><a href="#">Services</a></li>
+                                        <li><a href="#">Testimonial</a></li>
+                                        <li><a href="#">Blog</a></li>
+                                        <li><a href="#">Contact</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="widget_menu">
+                                    <h3>Account</h3>
+                                    <ul>
+                                        <li><a href="#">Account</a></li>
+                                        <li><a href="#">Checkout</a></li>
+                                        <li><a href="#">Login</a></li>
+                                        <li><a href="#">Register</a></li>
+                                        <li><a href="#">Shopping</a></li>
+                                        <li><a href="#">Widget</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-5">
+                        <div class="widget_menu">
+                            <h3>Newsletter</h3>
+                            <div class="information_f">
+                                <p>Subscribe by our newsletter and get update protidin.</p>
+                            </div>
+                            <div class="form_sub">
+                                <form>
+                                    <fieldset>
+                                        <div class="field">
+                                            <input type="email" placeholder="Enter Your Mail" name="email" />
+                                            <input type="submit" value="Subscribe" />
+                                        </div>
+                                    </fieldset>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- footer end -->
+<!--	<footer id="colophon" class="site-footer">-->
+<!--		<div class="site-info">-->
+<!--			<a href="--><?php //echo esc_url( __( 'https://wordpress.org/', 'storebase' ) ); ?><!--">-->
+<!--				--><?php
+//				/* translators: %s: CMS name, i.e. WordPress. */
+//				printf( esc_html__( 'Proudly powered by %s', 'storebase' ), 'WordPress' );
+//				?>
+<!--			</a>-->
+<!--			<span class="sep"> | </span>-->
+<!--				--><?php
+//				/* translators: 1: Theme name, 2: Theme author. */
+//				printf( esc_html__( 'Theme: %1$s by %2$s.', 'storebase' ), 'storebase', '<a href="https://wcstudio.com">WC Studio</a>' );
+//				?>
+<!--		</div>-->
+<!--	</footer>-->
+<!--</div>-->
 
 <?php wp_footer(); ?>
 
--- functions.php
+++ functions.php
@@ -47,12 +47,11 @@ function storebase_setup() {
 	add_theme_support( 'post-thumbnails' );
 
 	// This theme uses wp_nav_menu() in one location.
-	register_nav_menus(
-		array(
-			'menu-1' => esc_html__( 'Primary', 'storebase' ),
-		)
-	);
-
+    add_theme_support('menus');
+    register_nav_menus(array(
+            'primary' => __('Primary Menu', 'storebase'),
+            'footer'  => __('Footer Menu', 'storebase'),
+        ));
 	/*
 		* Switch default core markup for search form, comment form, and comments
 		* to output valid HTML5.
@@ -139,9 +138,18 @@ add_action( 'widgets_init', 'storebase_widgets_init' );
  */
 function storebase_scripts() {
 	wp_enqueue_style( 'storebase-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( 'storebase-style', 'rtl', 'replace' );
+    wp_enqueue_style( 'storebase-bootstrap', get_template_directory_uri() .'/assets/css/bootstrap.css', array(), _S_VERSION );
+    wp_enqueue_style( 'storebase-font-awesome', get_template_directory_uri() .'/assets/css/font-awesome.min.css', array(), _S_VERSION );
+    wp_enqueue_style( 'storebase-main', get_template_directory_uri() . '/assets/css/style.css', array(), _S_VERSION,'all' );
+    wp_enqueue_style( 'storebase-responsive', get_template_directory_uri() .'/assets/css/responsive.css', array(), _S_VERSION );
 
-	wp_enqueue_script( 'storebase-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+//    wp_style_add_data( 'storebase-style', 'rtl', 'replace' );
+
+//	wp_enqueue_script( 'storebase-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+    wp_enqueue_script( 'storebase-jquery', get_template_directory_uri() .'/assets/js/jquery-3.4.1.min.js', array(), _S_VERSION, true );
+    wp_enqueue_script( 'storebase-popper', get_template_directory_uri() .'/assets/js/popper.min.js', array(), _S_VERSION, true );
+    wp_enqueue_script( 'storebase-bootstrap', get_template_directory_uri() .'/assets/js/bootstrap.js', array(), _S_VERSION, true );
+    wp_enqueue_script( 'storebase-custom', get_template_directory_uri() .'/assets/js/custom.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
